2ac7935a6d04798061d3c7c2670b42aa248aa4c3
Jack <jack@lavnet.net>
2024-10-30 11:17:22 -0500

    loki<alloy and  infludb<telegraf working version

--- lab-vault.sh
+++ lab-vault.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-
+echo $1
 ansible-vault $1 ./ProvisionCollection/telegraf/files/etc-default-telegraf-cloud
-ansible-vault $1 ./ProvisionCollection/telegraf/files/etc-default-telegraf-local
+ansible-vault $1 ./ProvisionCollection/telegraf/files/etc-default-telegraf-homee
+
